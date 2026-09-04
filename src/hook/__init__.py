7fc3da64b76fa16f2295346446059bb4c914cf5c
Leila150 <esguerradominguita@gmail.com>
2026-09-04 19:15:22 +0800

    Expose advanced AI graphics and toolchain APIs

--- src/hook/__init__.py
+++ src/hook/__init__.py
@@ -1,17 +1,19 @@
-"""HOOK 1.0 public API."""
+"""HOOK 1.x public API."""
 from .engine import run, execute, compile_source, Lexer, Parser, Engine
 from .errors import HookError
 from .runtime_extensions import install_engine_extensions
-from .extension_system import (ExtensionBase, Type, Error, Statement, Loop, Operator, Language, Processor, ExtensionDefinition, ExtensionRegistry, ExtensionConflict, CATEGORIES, install_extension_system)
+from .extension_system import ExtensionBase, Type, Error, Statement, Loop, Operator, Language, Processor, ExtensionDefinition, ExtensionRegistry, ExtensionConflict, CATEGORIES, install_extension_system
 from .universal_runtime import install_universal_runtime
 from .extension_runtime import install_extension_runtime
 from .compiler import Compiler, VM, Program, Instruction
 from .language_core import ASTNode, ASTParser, SemanticAnalyzer, SemanticError, BytecodeCompiler, BytecodeProgram, LanguagePipeline
 from .persistence import HKD, HKDError, PersistentScope, PersistentScopes
 from .tooling import Formatter, Linter, Diagnostic, Profiler, TestRunner
-from .package_manager import Package, PackageManager
+from .package_manager import Package, PackageManager, Version, satisfies
 from .interop import NativeLibrary, CInterop, CppInterop, RustInterop, ABI
-from .ai_core import Tensor, tensor, softmax, argmax, relu, sigmoid, linear, Sequential, ModelRegistry
+from .ai_core import Tensor as CoreTensor, tensor, softmax, argmax, relu, sigmoid, linear, Sequential, ModelRegistry
+from .ai_training import Parameter, Optimizer, SGD, Adam, mse, binary_cross_entropy, Dataset
+from .ai_advanced import Tensor, Module, Linear, LayerNorm, MultiHeadAttention, FeedForward, TransformerBlock, TinyTransformer, CharTokenizer
 from .diagnostics_core import SourcePosition, TraceFrame, TracebackBuilder, SourceMap, DiagnosticEngine
 from .concurrency import CancellationToken, Channel, TaskGroup, Scheduler, Mutex, Atom
 from .memory import Pointer, MemoryManager, Unsafe
@@ -19,11 +21,11 @@ from .native_bindings import Ownership, Binding, NativeBindings
 from .vm_core import Op, Bytecode, VM as CoreVM
 from .web_framework import Request, Response, Router, WebApp
 from .game_runtime import Color, Vec2, Entity, Scene, Game, GUI, Audio
-from .ai_training import Parameter, Optimizer, SGD, Adam, mse, binary_cross_entropy, Dataset
-from .toolchain import TARGETS, Project, AndroidToolchain, Codegen
+from .toolchain import TARGETS, TRIPLES, Tool, find_tool, Project, AndroidToolchain, Codegen
 from .dialects import Dialect, DialectEngine, SyntaxExtension, ExtensionPipeline
+from .graphics3d import Vec3, Camera, Mesh, SoftwareRenderer, GPUBackend
+from .full_runtime import (TensorValue, ParameterValue, LinearLayer, ReLULayer, SoftmaxLayer, NeuralModel, SGDOptimizer, AdamOptimizer, mse_loss, cross_entropy, NativeToolchain, find_native_compiler, compile_c, load_library, CFFI, AndroidProject, GUIRuntime, GameEntity, GameRuntime, Database, TCPServer, format_hook, lint_hook, Debugger, LSPServer)
 from .completion import Position, Diagnostics, SourceAnalyzer, ExpressionVM, Value, TaskScheduler, AsyncChannel, ModuleSpec, ModuleResolver, NativeTarget, TargetResolver, Serializer, CompilationResult, CompletePipeline
-from .full_runtime import (TensorValue, ParameterValue, LinearLayer, ReLULayer, SoftmaxLayer, NeuralModel, SGDOptimizer, AdamOptimizer, mse_loss, cross_entropy, NativeToolchain, find_native_compiler, compile_c, load_library, CFFI, AndroidProject, GUIRuntime, GameEntity, GameRuntime, Database, TCPServer, format_hook, lint_hook, Profiler as FullProfiler, Debugger, LSPServer, ProjectManifest, PackageInstaller, SourceSpan, exception_report)
 from .v1_runtime import VERSION, LANGUAGE, Module, FeatureRegistry, install_v1_runtime
 from .v1_fixes import install_v1_fixes
 
@@ -35,11 +37,4 @@ install_v1_runtime(Engine)
 install_v1_fixes(Engine)
 
 __version__ = VERSION
-__all__ = [
-    "run", "execute", "compile_source", "Lexer", "Parser", "Engine", "Compiler", "VM", "Program", "Instruction", "HookError", "__version__", "VERSION", "LANGUAGE", "Module", "FeatureRegistry",
-    "ExtensionBase", "Type", "Error", "Statement", "Loop", "Operator", "Language", "Processor", "ExtensionDefinition", "ExtensionRegistry", "ExtensionConflict", "CATEGORIES",
-    "ASTNode", "ASTParser", "SemanticAnalyzer", "SemanticError", "BytecodeCompiler", "BytecodeProgram", "LanguagePipeline", "HKD", "HKDError", "PersistentScope", "PersistentScopes", "Formatter", "Linter", "Diagnostic", "Profiler", "TestRunner", "Package", "PackageManager", "NativeLibrary", "CInterop", "CppInterop", "RustInterop", "ABI",
-    "Tensor", "tensor", "softmax", "argmax", "relu", "sigmoid", "linear", "Sequential", "ModelRegistry", "SourcePosition", "TraceFrame", "TracebackBuilder", "SourceMap", "DiagnosticEngine", "CancellationToken", "Channel", "TaskGroup", "Scheduler", "Mutex", "Atom", "Pointer", "MemoryManager", "Unsafe", "Ownership", "Binding", "NativeBindings", "Op", "Bytecode", "CoreVM", "Request", "Response", "Router", "WebApp", "Color", "Vec2", "Entity", "Scene", "Game", "GUI", "Audio", "Parameter", "Optimizer", "SGD", "Adam", "mse", "binary_cross_entropy", "Dataset", "TARGETS", "Project", "AndroidToolchain", "Codegen", "Dialect", "DialectEngine", "SyntaxExtension", "ExtensionPipeline",
-    "Position", "Diagnostics", "SourceAnalyzer", "ExpressionVM", "Value", "TaskScheduler", "AsyncChannel", "ModuleSpec", "ModuleResolver", "NativeTarget", "TargetResolver", "Serializer", "CompilationResult", "CompletePipeline",
-    "TensorValue", "ParameterValue", "LinearLayer", "ReLULayer", "SoftmaxLayer", "NeuralModel", "SGDOptimizer", "AdamOptimizer", "mse_loss", "cross_entropy", "NativeToolchain", "find_native_compiler", "compile_c", "load_library", "CFFI", "AndroidProject", "GUIRuntime", "GameEntity", "GameRuntime", "Database", "TCPServer", "format_hook", "lint_hook", "FullProfiler", "Debugger", "LSPServer", "ProjectManifest", "PackageInstaller", "SourceSpan", "exception_report", "install_v1_runtime", "install_v1_fixes",
-]
+__all__ = [name for name in globals() if not name.startswith("_")]
