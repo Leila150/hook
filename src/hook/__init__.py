28e61d4de2edc3f011aa47bd585fd146dbebd96a
Leila150 <esguerradominguita@gmail.com>
2026-09-04 18:37:03 +0800

    Expose and install HOOK's complete extension system

--- src/hook/__init__.py
+++ src/hook/__init__.py
@@ -2,7 +2,21 @@
 from .engine import run, execute, compile_source, Lexer, Parser, Engine
 from .errors import HookError
 from .runtime_extensions import install_engine_extensions
+from .extension_system import (
+    ExtensionBase, Type, Error, Statement, Loop, Operator, Language, Processor,
+    ExtensionDefinition, ExtensionRegistry, ExtensionConflict, CATEGORIES,
+    install_extension_system,
+)
 from .compiler import Compiler, VM, Program, Instruction
+
 install_engine_extensions(Engine)
+install_extension_system(Engine)
+
 __version__="0.1.0"
-__all__=["run","execute","compile_source","Lexer","Parser","Engine","Compiler","VM","Program","Instruction","HookError","__version__"]
+__all__=[
+    "run","execute","compile_source","Lexer","Parser","Engine",
+    "Compiler","VM","Program","Instruction","HookError","__version__",
+    "ExtensionBase","Type","Error","Statement","Loop","Operator",
+    "Language","Processor","ExtensionDefinition","ExtensionRegistry",
+    "ExtensionConflict","CATEGORIES",
+]
